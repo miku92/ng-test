d1cb4f5c663a2497bc1f7b608083acab190cd090
miku92 <esposito.michele4@gmail.com>
2024-02-03 17:26:16 +0100

    cambio stato

--- .github/workflows/redis.js
+++ .github/workflows/redis.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const targetPath = '/src/environments/environment.prod.ts';
+const targetPath = 'src/environments/environments.prod.ts';
 const file = fs.readFileSync(targetPath ,'utf8');
 const newFile = file.replace('TESTOSECRET', process.env.TESTOSECRET);
 
